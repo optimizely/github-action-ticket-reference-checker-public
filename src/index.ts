a0a5d6307503ba04622ef37f55c24f4dfc3c97b3
shaharyarsheikh <shaharyarsheikh@folio3.com>
2022-07-19 11:55:19 +0500

    fixes

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,8 @@ async function run(): Promise<void> {
       }
     }
 
+    debug('bodyCheck', bodyCheck?.length ? bodyCheck.toString() : 'bodyCheck is null');
+
     if (bodyCheck === null && bodyURLCheck === null) {
       debug('failure', 'Title, branch, and body do not contain a reference to a ticket');
       setFailed('No ticket was referenced in this pull request');
